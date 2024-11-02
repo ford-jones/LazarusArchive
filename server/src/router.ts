ca38ff5ef350e0ad0bb1b8ff8a45a1702c772bde
ford-jones <fordnicholasjones@gmail.com>
2024-11-02 15:46:11 +1300

    Update: Connect to database

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,16 +1,33 @@
+import { db } from "./db"
+import { FindCursor, WithId, Document } from "mongodb"
+import { BlogPost } from "../types"
+
+import { StatusCodes } from "http-status-codes"
 import express from "express"
 import dotenv from "dotenv"
 dotenv.config()
 
 export const router: express.Router = express.Router()
 
-router.get("/getAllPosts", (req, res): void => {
-    res.status(200).json("Connected.")
-})
+router.get("/getAllPosts", async (req, res): Promise<void> => {
+    const database = await db()
+    const data = await database.getAll("posts")
 
-router.post("/addPost", (req, res): void => {
-    const data = JSON.parse(req.body)
+    const scanDocs: FindCursor<WithId<Document>> = data.map((blogPost: WithId<Document>) => blogPost)
+    const documents: Array<WithId<Document>> = await scanDocs.toArray()
+    
     console.log("Data: ", data)
 
-    res.status(200)
+    res.status(StatusCodes.OK).json(documents)
+})
+
+router.post("/addPost", async (req, res): Promise<void> => {
+    const database = await db()
+    
+    const data: BlogPost = req.body
+    await database.addDocument("posts", data)
+
+    console.log("Data: ", data)
+
+    res.status(StatusCodes.OK)
 })
