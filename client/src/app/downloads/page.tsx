580fc0374c947f0b7de173b84e6bae13bfa9e2d5
ford-jones <fordnicholasjones@gmail.com>
2024-11-11 13:39:46 +1300

    Update: Downloads

--- client/src/app/downloads/page.tsx
+++ client/src/app/downloads/page.tsx
@@ -18,6 +18,9 @@ export default function Downloads(): React.JSX.Element {
             <div style={{paddingTop: "105px"}}>
                     <ul>
                         <li>
+                            <a style={{color: "whitesmoke"}} href="https://github.com/ford-jones/Lazarus/archive/refs/tags/v0.1.3.zip">0.1.3-alpha</a>
+                        </li>
+                        <li>
                             <a style={{color: "whitesmoke"}} href="https://github.com/ford-jones/Lazarus/archive/refs/tags/v0.1.2.zip">0.1.2-alpha</a>
                         </li>
                     </ul>
